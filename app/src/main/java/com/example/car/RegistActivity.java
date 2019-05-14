9affa7cb8202bb51b09914f37acecb3b1ce9c88a
lzkmnd <44519840+lzkmnd@users.noreply.github.com>
2019-05-14 23:06:42 +0800

    V:3(连接carServer注册实现)

--- app/src/main/java/com/example/car/RegistActivity.java
+++ app/src/main/java/com/example/car/RegistActivity.java
@@ -1,27 +1,21 @@
 package com.example.car;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.Bitmap.Config;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request.Method;
 import com.android.volley.Response.ErrorListener;
 import com.android.volley.Response.Listener;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.ImageLoader;
-import com.android.volley.toolbox.ImageLoader.ImageListener;
-import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
@@ -29,20 +23,125 @@ import java.util.Map;
 public class RegistActivity extends Activity {
     private EditText id;
     private EditText password;
-    private String yanzheng;
+    private EditText email;
+    private EditText yanzhengma;
+    private int yanzheng;
 
-    private void volleyPostWithJsonObjectRequest() {
-        String url = "http://www.kuaidi100.com/query";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regist);
+        Button btn_regist = (Button)this.findViewById(R.id.btn_regist);
+        Button btn_yanzheng = (Button)this.findViewById(R.id.btn_yanzheng);
+        id = this.findViewById(R.id.regist_uname);
+        password = this.findViewById(R.id.regist_password);
+        yanzhengma = this.findViewById(R.id.regist_yanzheng);
+        email = this.findViewById(R.id.regist_email);
+        test();
+        btn_regist.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String loginId = id.getText().toString();
+                String pass = password.getText().toString();
+                String yanzheng1 = yanzhengma.getText().toString();
+                Integer yan = Integer.parseInt(yanzheng1);
+                regist(loginId,pass,yan);
+            }
+        });
+        btn_yanzheng.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String emial = email.getText().toString();
+                getYanZheng(emial);
+            }
+        });
+    }
+
+    public void regist(String userid ,String password,int yan){
+        if(yan==yanzheng){
+            String url = "http://192.168.191.1:8080/carServer/Regist";
+            Map<String,String> map = new HashMap<>();
+            map.put("userid",userid);
+            map.put("password",password);
+            //将map转化为JSONObject对象
+            JSONObject jsonObject = new JSONObject(map);
+            Log.i("json","注册（）......");
+            JsonObjectRequest request = new JsonObjectRequest(Method.POST, url, jsonObject,
+                    new Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject jsonObject) {//jsonObject为请求返回的Json格式数据
+                            String result = null;
+                            try {
+                                result = jsonObject.getJSONObject("result").toString();
+                                if("true".equals(result)){
+                                    Toast.makeText(RegistActivity.this,"注册成功",Toast.LENGTH_LONG).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            Log.i("json",result);
+                        }
+                    },
+                    new ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError volleyError) {
+                            Toast.makeText(RegistActivity.this,volleyError.toString()+" 注册失败",Toast.LENGTH_LONG).show();
+                        }
+                    });
+            //设置请求的Tag标签，可以在全局请求队列中通过Tag标签进行请求的查找
+            request.setTag("testPost");
+            MyApplication.getHttpQueues().add(request);
+        }else{
+            Toast.makeText(RegistActivity.this,"验证码错误",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void test() {
+        String url = "http://192.168.191.1:8080/carServer/getOneCategory";
         Map<String,String> map = new HashMap<>();
-        map.put("type","yuantong");
-        map.put("postid","229728279823");
+        map.put("userid","zhangsan");
+        map.put("password","112233");
         //将map转化为JSONObject对象
         JSONObject jsonObject = new JSONObject(map);
-
+        Log.i("json","test连接开始");
         JsonObjectRequest request = new JsonObjectRequest(Method.POST, url, jsonObject,
                 new Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject jsonObject) {//jsonObject为请求返回的Json格式数据
+                        Log.i("json",jsonObject.toString() );
+
+                        Toast.makeText(RegistActivity.this,jsonObject.toString(),Toast.LENGTH_LONG).show();
+                    }
+                },
+                new ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        Log.i("fail",volleyError.toString() );
+                        Toast.makeText(RegistActivity.this,volleyError.toString(),Toast.LENGTH_LONG).show();
+                    }
+                });
+        //设置请求的Tag标签，可以在全局请求队列中通过Tag标签进行请求的查找
+        request.setTag("testPost");
+        //将请求加入全局队列中
+        MyApplication.getHttpQueues().add(request);
+    }
+
+    private void getYanZheng(String email) {
+        String url = "http://192.168.191.1:8080/carServer/getYanzheng";
+        Map<String,String> map = new HashMap<>();
+        map.put("email",email);
+        //将map转化为JSONObject对象
+        JSONObject jsonObject = new JSONObject(map);
+        Log.i("json","请求验证码....");
+        JsonObjectRequest request = new JsonObjectRequest(Method.POST, url, jsonObject,
+                new Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject jsonObject) {//jsonObject为请求返回的Json格式数据
+                        Log.i("json",jsonObject.toString() );
+                        try {
+                            String yanzhengmStr = jsonObject.get("yanzhengma").toString();
+                            yanzheng = Integer.valueOf(yanzhengmStr);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                         Toast.makeText(RegistActivity.this,jsonObject.toString(),Toast.LENGTH_LONG).show();
                     }
                 },
@@ -56,27 +155,10 @@ public class RegistActivity extends Activity {
         request.setTag("testPost");
         //将请求加入全局队列中
         MyApplication.getHttpQueues().add(request);
+        //return Integer.parseInt(jsonObject.toString());
     }
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_regist);
-        Button btn_regist = (Button)this.findViewById(R.id.btn_regist);
-        id = this.findViewById(R.id.regist_uname);
-        password = this.findViewById(R.id.regist_password);
-        id = this.findViewById(R.id.regist_uname);
-        password = this.findViewById(R.id.regist_password);
-        volleyPostWithJsonObjectRequest();
-        btn_regist.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String loginId = id.getText().toString();
-                String pass = password.getText().toString();
-
-            }
-        });
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
